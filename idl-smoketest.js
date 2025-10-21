0352841ffd09d4bedd2a1751144ad1107a41c030
Rich Trott <rtrott@gmail.com>
2025-10-21 13:21:39 -0700

    add tests for section 6 of the smoke test procedure
    In each of the industries, click on the tabs (About, Documents, etc.)
    and make sure they load.
    
    In previous tests, it was noted that Browse didn't work for Fossil Fuels,
    so we're also testing the Browse link (in the About page) for each
    industry.

--- idl-smoketest.js
+++ idl-smoketest.js
@@ -2,7 +2,6 @@
 
 import puppeteer from 'puppeteer'
 import { test, describe } from 'node:test'
-import { availableParallelism } from 'os'
 
 const IDL_URL = process.env.IDL_URL || 'http://localhost:4173/'
 const HEADLESS = !process.env.IDL_SHOWBROWSER
@@ -25,7 +24,7 @@ const tests = [
     description: 'Check home page',
     test: async (page) => {
       await page.$eval('body', body => {
-        if (!body.innerText.includes('Industry Documents Library')) {
+        if (!body.innerText.includes('Indus.   try Documents Library')) {
           throw new Error('Text "Industry Documents Library" not found')
         }
       })
@@ -68,37 +67,6 @@ const tests = [
           throw new Error('Text "User feedback" not found')
         }
       })
-    }
-  },
-
-  // Check external header/footer links
-  {
-    description: 'Check external header/footer links',
-    test: async (page) => {
-      const bannerChecks = [
-        { clickSelector: '.header-ucsf nav a::-p-text(About UCSF)', expected: /^https:\/\/www\.ucsf\.edu\/about$/ },
-        { clickSelector: '.header-ucsf nav a::-p-text(Search UCSF)', expected: /^https:\/\/www\.ucsf\.edu\/search$/ },
-        { clickSelector: '.header-ucsf nav a::-p-text(UCSF Health)', expected: /^https:\/\/www\.ucsfhealth\.org\/$/ },
-        { clickSelector: '.header-ucsf img', expected: /^https:\/\/www\.ucsf\.edu\/$/ },
-        { clickSelector: '.header-idl nav ::-p-text(Industry Documents Library)', expected: /^https?:\/\/[a-zA-Z0-9:.-]+\/(home\/)?$/ },
-        { clickSelector: '.header-idl nav ::-p-text(Industries)', expected: /^https?:\/\/[a-zA-Z0-9:.-]+\/(home\/)?$/ },
-        { clickSelector: 'footer .logo', expected: /^https?:\/\/www\.library\.ucsf\.edu\/$/ },
-        { clickSelector: 'footer img[alt="Instagram"]', expected: /https.+www\.instagram\.com.+ucsf_industrydocs/ },
-        { clickSelector: 'footer img[alt="Bluesky"]', expected: /^https:\/\/bsky\.app\/profile\/ucsf-industrydocs\.bsky\.social$/ },
-        { clickSelector: 'footer img[alt="YouTube"]', expected: /^https:\/\/www\.youtube\.com\/@ucsfindustrydocumentslibrary\/videos$/ },
-        { clickSelector: 'footer img[alt="LinkedIn"]', expected: /^https:\/\/www\.linkedin\.com\/company\/industry-documents-library$/ }
-      ]
-
-      for (const check of bannerChecks) {
-        await page.goto(IDL_URL, { waitUntil: 'networkidle2' })
-        await page.waitForSelector('.header-ucsf')
-        await page.locator(check.clickSelector).click()
-        await page.waitForNavigation()
-        const currentUrl = await page.evaluate('window.location')
-        if (check.expected.test(currentUrl.href.replace(/#.*$/, '')) === false) {
-          throw new Error(`Expected URL to match "${check.expected}", but got "${currentUrl.href}"`)
-        }
-      }
     }
   },
 
@@ -239,23 +207,109 @@ const tests = [
         if (top.length > 1) {
           throw new Error(`Expected only one visible .page-tools-top, but found ${top.length}`)
         }
-        if (!/\d+\s*Results/.test(top[0].innerText)) {
-          throw new Error(`Expected text matching /\\d+\\s*Results/ in .page-tools-top, but got "${top[0].innerText}"`)
+        if (!/\d{3}\s*Results/.test(top[0].innerText)) {
+          throw new Error(`Expected text matching /\\d{3}\\s*Results/ in .page-tools-top, but got "${top[0].innerText}"`)
         }
       })
+    }
+  },
+
+  {
+    description: 'Test tabs in each industry page',
+    test: async (page) => {
+      const assertResultsVisible = async () => {
+        await page.waitForSelector('.page-tools-top ::-p-text(Results)')
+        await page.$$eval('.page-tools-top', (elements) => {
+          const top = elements.filter(el => el.checkVisibility())
+          if (top.length === 0) {
+            throw new Error('Can not find visible .page-tools-top')
+          }
+          if (top.length > 1) {
+            throw new Error(`Expected only one visible .page-tools-top, but found ${top.length}`)
+          }
+          if (!/\d{2}\s*Results/.test(top[0].innerText)) {
+            throw new Error(`Expected text matching /\\d{2}\\s*Results/ in .page-tools-top, but got "${top[0].innerText}"`)
+          }
+        })
+      }
+
+      const industryTabs = [
+        { name: 'Documents', check: assertResultsVisible },
+        { name: 'About', selector: 'h1 ::-p-text(About)' },
+        { name: 'Publications', check: assertResultsVisible },
+        { name: 'Collections', selector: 'h1 ::-p-text(Collections)' },
+        { name: 'More Resources', selector: 'h1 ::-p-text(More Resources)' }
+      ]
+
+      const industries = ['Tobacco', 'Opioids', 'Chemical', 'Drug', 'Food', 'Fossil Fuel']
+      for (const industry of industries) {
+        await page.waitForSelector('.industry-menu')
+        await page.locator(`.industry-menu ::-p-text(${industry})`).click()
+        await page.waitForSelector(`h2 ::-p-text(${industry} Industry)`)
+        await page.waitForSelector('#industry-navbar')
+        for (const tab of industryTabs) {
+          const needsNav = !tab.selector
+          await page.locator(`#industry-navbar ::-p-text(${tab.name})`).click()
+          if (needsNav) {
+            await page.waitForNavigation({ waitUntil: 'networkidle2' })
+          }
+          await page.waitForSelector(`#industry-navbar .nav-link.active ::-p-text(${tab.name})`)
+          if (tab.check) {
+            await tab.check()
+          } else if (tab.selector) {
+            await page.waitForSelector(tab.selector)
+          }
+          // Check the "Browse" link on the About page.
+          if (tab.name === 'About') {
+            await page.locator(`.link-container ::-p-text(Browse Popular ${industry} Documents)`).click()
+            await page.waitForNavigation({ waitUntil: 'networkidle2' })
+            await page.waitForSelector('#industry-navbar .nav-link.active ::-p-text(Documents)')
+            assertResultsVisible()
+          }
+        }
+        await page.goto(IDL_URL, { waitUntil: 'networkidle2' })
+      }
+    }
+  },
+
+  {
+    description: 'Check external header/footer links',
+    test: async (page) => {
+      const bannerChecks = [
+        { clickSelector: '.header-ucsf nav a::-p-text(About UCSF)', expected: /^https:\/\/www\.ucsf\.edu\/about$/ },
+        { clickSelector: '.header-ucsf nav a::-p-text(Search UCSF)', expected: /^https:\/\/www\.ucsf\.edu\/search$/ },
+        { clickSelector: '.header-ucsf nav a::-p-text(UCSF Health)', expected: /^https:\/\/www\.ucsfhealth\.org\/$/ },
+        { clickSelector: '.header-ucsf img', expected: /^https:\/\/www\.ucsf\.edu\/$/ },
+        { clickSelector: '.header-idl nav ::-p-text(Industry Documents Library)', expected: /^https?:\/\/[a-zA-Z0-9:.-]+\/(home\/)?$/ },
+        { clickSelector: '.header-idl nav ::-p-text(Industries)', expected: /^https?:\/\/[a-zA-Z0-9:.-]+\/(home\/)?$/ },
+        { clickSelector: 'footer .logo', expected: /^https?:\/\/www\.library\.ucsf\.edu\/$/ },
+        { clickSelector: 'footer img[alt="Instagram"]', expected: /https.+www\.instagram\.com.+ucsf_industrydocs/ },
+        { clickSelector: 'footer img[alt="Bluesky"]', expected: /^https:\/\/bsky\.app\/profile\/ucsf-industrydocs\.bsky\.social$/ },
+        { clickSelector: 'footer img[alt="YouTube"]', expected: /^https:\/\/www\.youtube\.com\/@ucsfindustrydocumentslibrary\/videos$/ },
+        { clickSelector: 'footer img[alt="LinkedIn"]', expected: /^https:\/\/www\.linkedin\.com\/company\/industry-documents-library$/ }
+      ]
+
+      for (const check of bannerChecks) {
+        await page.goto(IDL_URL, { waitUntil: 'networkidle2' })
+        await page.waitForSelector('.header-ucsf')
+        await page.locator(check.clickSelector).click()
+        await page.waitForNavigation()
+        const currentUrl = await page.evaluate('window.location')
+        if (check.expected.test(currentUrl.href.replace(/#.*$/, '')) === false) {
+          throw new Error(`Expected URL to match "${check.expected}", but got "${currentUrl.href}"`)
+        }
+      }
     }
   }
 ]
 
-describe('IDL Smoke Tests', {concurrency: 4}, async () => {
+describe('IDL Smoke Tests', { concurrency: true }, async () => {
   for (const currentTest of tests) {
     const fn = currentTest
     test(currentTest.description, async () => {
       const { browser, page } = await setup()
       try {
         await fn.test(page)
-      } catch (err) {
-        console.error(err)
       } finally {
         await teardown(browser)
       }
